067504bb61b462b1101819cd22fee2c2936a3bb2
fangsxin <45865656+fangsxin@users.noreply.github.com>
2019-02-12 23:48:11 +0800

    Add files via upload

---
+++ 新哥博客/文章/home/js/scrolltop.js
@@ -0,0 +1,19 @@
+﻿function goTopEx() {
+    var obj = document.getElementById("goTopBtn");
+    function getScrollTop() {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        return scrollTop
+    }
+    function setScrollTop(value) {
+        document.documentElement.scrollTop = value;
+        document.body.scrollTop = value;
+    }
+    window.onscroll = function () { getScrollTop() > 500 ? obj.style.display = "block" : obj.style.display = "none"; }
+    obj.onclick = function () {
+        var goTop = setInterval(scrollMove, 10);
+        function scrollMove() {
+            setScrollTop(getScrollTop() / 1.1);
+            if (getScrollTop() < 1) clearInterval(goTop);
+        }
+    }
+}
---
+++ 新哥博客/文章/home/js/ucommon.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    $('.btn_set').bind("click", function () {
+        $('.mod_tieset_fade').toggleClass("db"),
+        $('.mod_cover').toggleClass("db")
+    });
+    //点击取消遮罩
+    $(".mod_cover").bind("touchstart", function () {
+        $('.mod_tieset_fade').removeClass('db'),
+        $('.mod_cover').removeClass("db")
+    });
+})
+
+//回到顶部
+function goTopEx() {
+    var obj = document.getElementById("goTopBtn");
+    function getScrollTop() {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        return scrollTop
+    }
+    function setScrollTop(value) {
+        document.documentElement.scrollTop = value;
+        document.body.scrollTop = value;
+    }
+    window.onscroll = function () { getScrollTop() > 0 ? obj.style.display = "" : obj.style.display = "none"; }
+    obj.onclick = function () {
+        var goTop = setInterval(scrollMove, 10);
+        function scrollMove() {
+            setScrollTop(getScrollTop() / 1.1);
+            if (getScrollTop() < 1) clearInterval(goTop);
+        }
+    }
+}
